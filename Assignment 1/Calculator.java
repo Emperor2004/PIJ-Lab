bb2b780663a3eb09df7d92f617a4d6410b49458a
Emperor <panditom2003@gmail.com>
2025-01-22 23:34:23 +0530

    Updated Calculator

--- Assignment 1/Calculator.java
+++ Assignment 1/Calculator.java
@@ -3,7 +3,7 @@ class Calculator {
         int[] num;
         UserInput in = new UserInput();
 
-        num = in.userInput();
+        num = in.userInputDouble();
         int sum = num[0] + num[1];
 
         return sum;
@@ -13,7 +13,7 @@ class Calculator {
         int[] num;
         UserInput in = new UserInput();
 
-        num = in.userInput();
+        num = in.userInputDouble();
         int diff = num[0] - num[1];
 
         return diff;
@@ -23,9 +23,65 @@ class Calculator {
         int[] num;
         UserInput in = new UserInput();
 
-        num = in.userInput();
+        num = in.userInputDouble();
         int pro = num[0] * num[1];
 
         return pro;
+    }
+
+    double division() {
+        int[] num;
+        UserInput in = new UserInput();
+
+        num = in.userInputDouble();
+        if (num[1] == 0) {
+            System.out.println("Can't divide by 0");
+            return 0;
+        }
+        int quo = num[0] / num[1];
+        return quo;
+    }
+
+    int[] fibonacci() {
+        int num;
+        UserInput in = new UserInput();
+
+        System.out.println("Enter no. of items:");
+        num = in.userInputSingle();
+
+        int[] arr = new int[num];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i < num; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+        
+        return arr;
+    }
+
+    double mean() {
+        UserInput in = new UserInput();
+        int[] array = in.userInputArray();
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return (double) sum / array.length;
+    }
+
+    double variance() {
+        UserInput in = new UserInput();
+        int[] array = in.userInputArray();
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        double mean = (double) sum / array.length;
+        double sumSquaredDiff = 0;
+        for (int num : array) {
+            sumSquaredDiff += Math.pow(num - mean, 2);
+        }
+        return sumSquaredDiff / array.length;
     }
 }
