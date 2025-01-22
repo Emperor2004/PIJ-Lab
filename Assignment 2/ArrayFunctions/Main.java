bac9494f544b600fef1e58fffef9504c3bdb7a07
Emperor <panditom2003@gmail.com>
2025-01-23 00:23:42 +0530

    A2 Main

---
+++ Assignment 2/ArrayFunctions/Main.java
@@ -0,0 +1,10 @@
+
+class Main {
+
+    public static void main(String[] args) {
+        UserInput in = new UserInput();
+        ArrayFunctions functions = new ArrayFunctions();
+
+        functions.diffInt(in.arrayInput());
+    }
+}
