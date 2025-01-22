93cb7b8a97a102f72f1e9384c95149899cf6a950
Emperor <panditom2003@gmail.com>
2025-01-22 22:51:53 +0530

    A1 UserInput

---
+++ Assignment 1/UserInput.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class UserInput {
+
+    int[] userInput() {
+        int[] numbers = new int[2];
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter 1st Number: ");
+        numbers[0] = scan.nextInt();
+
+        System.out.println("Enter 2nd Number: ");
+        numbers[1] = scan.nextInt();
+
+        return numbers;
+    }
+}
