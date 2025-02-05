3f5a0a51a2b3f9ca7d84f0118e24c0c40d59f06c
Emperor <panditom2003@gmail.com>
2025-02-05 15:16:08 +0530

    A4 Deck

---
+++ Assignment 4/Deck.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+class Deck {
+    ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<>();
+    }
+
+    public void createDeck() {
+        String[] ranks ={"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", "A"};
+        String[] suits ={"Hearts", "Clubs", "Diamonds", "Spades"};
+
+        for(String rank : ranks) {
+            for(String suit : suits) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
+}
