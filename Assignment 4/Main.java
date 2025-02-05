f784d6497c2dbb19fcd3ca576dc495db9a15c65c
Emperor <panditom2003@gmail.com>
2025-02-05 15:23:45 +0530

    A4 Main

---
+++ Assignment 4/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        System.out.println("------------Creating Deck---------------");
+        deck.createDeck();
+        System.out.println("------------Deck Created---------------");
+        System.out.println("------------Printing Deck---------------");
+        deck.displayDeck();
+    }
+}
