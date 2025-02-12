d1fca93c4d4c52ed9eddae3370d6648557269a99
Emperor <panditom2003@gmail.com>
2025-02-12 16:07:17 +0530

    Updated Main.java

--- Assignment 5/Main.java
+++ Assignment 5/Main.java
@@ -17,16 +17,29 @@ class Main {
         // System.out.println("Number of sides in Rectangle: " + rectangle.getNumSides());
         // System.out.println("-------------------------");
 
-        System.out.println("Enter radius:");
-        double radius = Double.parseDouble(scan.nextLine());
+        // System.out.println("Enter radius:");
+        // double radius = Double.parseDouble(scan.nextLine());
 
-        Shape circle = new Circle(radius);
-        System.out.println("-------------------------");
-        System.out.println("Area of Circle: " + circle.calculateArea());
-        System.out.println("Perimeter of Circle: " + Math.floor(circle.calculatePerimeter()));
-        System.out.println("Number of sides in Circle: " + circle.getNumSides());
-        System.out.println("-------------------------");
+        // Shape circle = new Circle(radius);
+        // System.out.println("-------------------------");
+        // System.out.println("Area of Circle: " + Math.floor(circle.calculateArea()));
+        // System.out.println("Perimeter of Circle: " + Math.floor(circle.calculatePerimeter()));
+        // System.out.println("Number of sides in Circle: " + circle.getNumSides());
+        // System.out.println("-------------------------");
 
+        System.out.println("Enter Side a:");
+        double a = Double.parseDouble(scan.nextLine());
+        System.out.println("Enter Side b:");
+        double b = Double.parseDouble(scan.nextLine());
+        System.out.println("Enter Side c:");
+        double c = Double.parseDouble(scan.nextLine());
+
+        Shape triangle = new Triangle(a, b, c);
+        System.out.println("-------------------------");
+        System.out.println("Area of Triangle: " + Math.floor(triangle.calculateArea()));
+        System.out.println("Perimeter of Triangle: " + triangle.calculatePerimeter());
+        System.out.println("Number of sides in Triangle: " + triangle.getNumSides());
+        System.out.println("-------------------------");
 
     }
 
