568a4bef08c303abc3176ac3b67bc23dcb5999eb
Emperor <panditom2003@gmail.com>
2025-02-26 15:45:53 +0530

    Some Changes

--- Assignment 7/Main.java
+++ Assignment 7/Main.java
@@ -1,21 +1,36 @@
+
+import java.util.Scanner;
+
+
 class Main {
 
     public static void checkAge(int age) throws AgeException {
         if (age < 18) {
-            throw new AgeException("Age not qualified");
+            throw new AgeException("Age not qualified...");
         }
         else {
             System.out.println("Valid...");
         }
     }
-    public static void main(String args[]) {
-        try {
-            checkAge(12);
-        } catch (AgeException e) {
-            System.out.println(e);
+
+    public static void notZero(int n) throws ArithmeticException {
+        if (n == 0) {
+            throw new ArithmeticException("Number can't be zero");
+        } else {
+            System.out.println("Valid...");
         }
-        finally {
-            System.out.println("Done.");
+    }
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            int a = scan.nextInt();
+            notZero(a);
+            int b = scan.nextInt();
+            notZero(b);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
