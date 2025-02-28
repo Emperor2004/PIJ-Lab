81469f161697c309a3cf417d3aa755748388b3f8
Emperor <panditom2003@gmail.com>
2025-03-01 01:05:17 +0530

    A3 Student

--- Assignment 3/Student.java
+++ Assignment 3/Student.java
@@ -1,32 +1,31 @@
-// Student.java 
+// Class representing a student with attributes and methods
 
-class Student{
-	private String name;
-	private long prn;
+class Student {
+    private String name;
+    private long prn;
 
-	public Student(String name, long prn){
-		setName(name);
-		setPRN(prn);
-	}
+    public Student(String name, long prn) {
+        this.name = name;
+        this.prn = prn;
+    }
 
-	public void setName(String name){
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getName(){
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setPRN(long prn){
-		this.prn = prn;
-	}
+    public void setPRN(long prn) {
+        this.prn = prn;
+    }
 
-	public long getPRN(){
-		return prn;
-	}
+    public long getPRN() {
+        return prn;
+    }
 
-	public void display(){
-		System.out.println("Name: "+getName()+" PRN: "+getPRN());
-	}
+    public void display() {
+        System.out.println("Name: " + name + ", PRN: " + prn);
+    }
 }
-
