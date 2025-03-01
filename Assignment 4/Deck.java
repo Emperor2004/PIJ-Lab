8b476fd425eb8f8d64e579b04cf3f131d72e0366
Emperor <panditom2003@gmail.com>
2025-03-01 23:58:01 +0530

    A4 Deck

--- Assignment 4/Deck.java
+++ Assignment 4/Deck.java
@@ -2,41 +2,7 @@
 import java.util.*;
 
 class Deck {
+
     ArrayList<Card> deck;
 
-    public Deck() {
-        deck = new ArrayList<>();
-    }
-
-    public void createDeck() {
-        String[] ranks ={"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q", "A"};
-        String[] suits ={"Hearts", "Clubs", "Diamonds", "Spades"};
-
-        for(String rank : ranks) {
-            for(String suit : suits) {
-                deck.add(new Card(rank, suit));
-            }
-        }
-    }
-
-    public void displayDeck() {
-        for (Card card : deck) {
-            System.out.println(card);
-        }
-    }
-
-    public void shuffleDeck() {
-        Collections.shuffle(deck);
-    }
-
-    // Select two random cards from the deck
-    public void randomCard() {
-        System.out.println(deck.get(1));
-        System.out.println(deck.get(2));
-    }
-
-    // Create 3 players and distribute 5 cards at random to player and display cards of all players
-    public void cardPlayers() {
-
-    }
 }
