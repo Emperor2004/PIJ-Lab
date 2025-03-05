599373436db01c5da0e904a76dc522afba539e43
Emperor <panditom2003@gmail.com>
2025-03-05 21:34:22 +0530

    A8

---
+++ Assignment 8 copy/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StudentOperations studentOperations = new StudentOperations();
+        studentOperations.insertStudent();
+        studentOperations.viewStudent();
+    }
+}
---
+++ Assignment 8 copy/StudentOperations.java
@@ -0,0 +1,67 @@
+
+import java.sql.*;
+import java.util.*;
+
+public class StudentOperations {
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static void insertStudent() {
+        try 
+        (
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);  
+            PreparedStatement stmt = conn.prepareStatement("INSERT into students (PRN, Name, Branch, Batch, CGPA) VALUES (?, ?, ?, ?, ?)")
+        )
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter PRN: ");
+            int prn = scan.nextInt();
+            scan.nextLine();
+            System.out.println("Enter Name: ");
+            String name = scan.nextLine();
+            System.out.println("Enter Branch: ");
+            String branch = scan.nextLine();
+            System.out.println("Enter Batch: ");
+            String batch = scan.nextLine();
+            System.out.println("Enter CGPA: ");
+            float cgpa = scan.nextFloat();
+
+            stmt.setInt(1, prn);
+            stmt.setString(2, name);
+            stmt.setString(3, branch);
+            stmt.setString(4, batch);
+            stmt.setFloat(5, cgpa);
+
+            int numberRows = stmt.executeUpdate();
+            if (numberRows > 1) {
+                System.out.println("Student Data Inserted.");
+            }
+        }
+        catch (SQLException e) {
+            System.err.println(e);
+        }
+    }
+
+    public static void viewStudent() {
+        try
+        (
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);  
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from students");
+        ) {
+            System.out.println("-------------------Student Records-------------------");
+            while(rs.next()){
+                System.out.println("PRN: " + rs.getInt("PRN") +
+                        ", Name: " + rs.getString("Name") +
+                        ", Branch: " + rs.getString("Branch") +
+                        ", Batch: " + rs.getString("Batch") +
+                        ", CGPA: " + rs.getFloat("CGPA"));
+            }
+            System.out.println("-----------------------------------------------------");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
