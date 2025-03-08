014bd8d65cae682188481aa99b5a6ddea7788193
Emperor <panditom2003@gmail.com>
2025-03-08 20:24:40 +0530

    Created RubberDuck class

---
+++ Assignment 6/RubberDuck.java
@@ -0,0 +1,14 @@
+
+class RubberDuck extends Duck {
+
+    public RubberDuck() {
+        setFlyBehavior(new FlyNoWay());
+        setQuackBehavior(new Squeak());
+        setSwimBehavior(new Float());
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a Rubber Duck");
+    }
+}
