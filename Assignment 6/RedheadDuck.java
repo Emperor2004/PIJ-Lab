a841bfdac0d309f92badb0c8f4441cb4a7397aec
Emperor <panditom2003@gmail.com>
2025-03-08 20:23:13 +0530

    Created RedheadDuck class

---
+++ Assignment 6/RedheadDuck.java
@@ -0,0 +1,14 @@
+
+class RedheadDuck extends Duck {
+
+    public RedheadDuck() {
+        setFlyBehavior(new FlyWithWings());
+        setQuackBehavior(new Quack());
+        setSwimBehavior(new Swim());
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am Redhead Duck");
+    }
+}
