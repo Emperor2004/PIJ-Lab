c3012b8165103724713a9b8ecf8ffd2c52f3d6ca
Emperor <panditom2003@gmail.com>
2025-03-08 20:25:30 +0530

    Created DecoyDuck class

---
+++ Assignment 6/DecoyDuck.java
@@ -0,0 +1,14 @@
+
+class DecoyDuck extends Duck {
+
+    public DecoyDuck() {
+        setFlyBehavior(new FlyNoWay());
+        setQuackBehavior(new MuteQuack());
+        setSwimBehavior(new Drown());
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Decoy Duck");
+    }
+}
