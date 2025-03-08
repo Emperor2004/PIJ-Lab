9e78c1891ad2cba39a0d2b84b4d4a3e35cd26810
Emperor <panditom2003@gmail.com>
2025-03-08 20:21:22 +0530

    Created MallardDuck class

---
+++ Assignment 6/MallardDuck.java
@@ -0,0 +1,14 @@
+
+class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        setFlyBehavior(new FlyWithWings());
+        setQuackBehavior(new Quack());
+        setSwimBehavior(new Swim());
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am Mallard Duck");
+    }
+}
