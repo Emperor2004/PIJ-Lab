438f868b21b3d377d9cf4246b4999be3ecd8f5de
Emperor <panditom2003@gmail.com>
2025-04-22 02:27:39 +0530

    Created Main class

---
+++ Assignment 10/Main.java
@@ -0,0 +1,22 @@
+/*
+Name: Om Narayan Pandit
+PRN: 23070126083
+Batch: AIML B1
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread started.");
+                
+        //creating the objects from the MyThread.java class
+        MyThread thread1 = new MyThread("Thread-1");
+        MyThread thread2 = new MyThread("Thread-2");
+        
+        
+        //starting those threads
+        thread1.start();
+        thread2.start();
+
+        System.out.println("Main thread ended.");
+    }
+}
