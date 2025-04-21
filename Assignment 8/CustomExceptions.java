2ac5b29230d89c6f12d9351f270edce0baddbd06
Emperor <panditom2003@gmail.com>
2025-04-21 15:26:24 +0530

    Added comments

--- Assignment 8/CustomExceptions.java
+++ Assignment 8/CustomExceptions.java
@@ -1,21 +1,25 @@
+// EmptyFieldException for empty fields
 class EmptyFieldException extends Exception {
     public EmptyFieldException(String message) {
         super(message);
     }
 }
 
+// DuplicatePRNException for duplicate PRN numbers
 class DuplicatePRNException extends Exception {
     public DuplicatePRNException(String message) {
         super(message);
     }
 }
 
+// StudentNotFoundException for student not found
 class StudentNotFoundException extends Exception {
     public StudentNotFoundException(String message) {
         super(message);
     }
 }
 
+// InvalidMarksException for invalid marks
 class InvalidMarksException extends Exception {
         public InvalidMarksException(String message) {
             super(message);
