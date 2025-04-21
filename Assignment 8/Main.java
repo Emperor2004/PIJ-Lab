a8986844b29246237232c846b53ae39ba444b255
Emperor <panditom2003@gmail.com>
2025-04-21 14:52:37 +0530

    Created Student class

--- Assignment 8/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StudentOperations studentOperations = new StudentOperations();
-        studentOperations.insertStudent();
-        studentOperations.viewStudent();
-    }
-}
