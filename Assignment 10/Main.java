a1107c9cc2d1120d8142d89271b93d0954fec3b5
Emperor <panditom2003@gmail.com>
2025-04-22 03:03:54 +0530

    Added comments

--- Assignment 10/Main.java
+++ Assignment 10/Main.java
@@ -1,22 +1,25 @@
 /*
-Name: Om Narayan Pandit
-PRN: 23070126083
-Batch: AIML B1
+ Name: Om Narayan Pandit
+ PRN: 23070126083
+ Batch: AIML B1
 */
 
+// Main.java
 public class Main {
     public static void main(String[] args) {
+        // Log the start of the main thread
         System.out.println("Main thread started.");
                 
-        //creating the objects from the MyThread.java class
+        // Create two MyThread instances with unique names
         MyThread thread1 = new MyThread("Thread-1");
         MyThread thread2 = new MyThread("Thread-2");
         
-        
-        //starting those threads
+        // Start both threads; they will execute run() concurrently
         thread1.start();
         thread2.start();
 
+        // Log that the main thread has finished its own execution
+        // Note: child threads may still be running after this line
         System.out.println("Main thread ended.");
     }
 }
--- Assignment 10/MyThread.java
+++ Assignment 10/MyThread.java
@@ -1,22 +1,38 @@
+// MyThread.java
 public class MyThread extends Thread {
+    // Name of this thread for logging purposes
     private String threadName;
 
+    /**
+     * Constructor to set up thread with a descriptive name
+     * @param name Thread name to display in console logs
+     */
     public MyThread(String name) {
         this.threadName = name;
     }
 
+    /**
+     * Entry point of the thread when start() is called.
+     * Prints a start message, counts from 1 to 5 with delays, and prints a finish message.
+     */
     @Override
     public void run() {
+        // Notify that this thread has started running
         System.out.println(threadName + " started running.");
+        
+        // Perform five iterations, logging the count each time
         for (int i = 1; i <= 5; i++) {
             System.out.println(threadName + ": " + i);
             try {
-              //sleep for 5ms
-                Thread.sleep(500);  
+                // Sleep for half a second between iterations to simulate work
+                Thread.sleep(500);
             } catch (InterruptedException e) {
+                // Handle the case where the thread is interrupted during sleep
                 System.out.println(threadName + " was interrupted.");
             }
         }
+
+        // Notify that this thread has completed its task
         System.out.println(threadName + " finished.");
     }
 }
