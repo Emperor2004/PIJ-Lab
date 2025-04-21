e4b3330affbb9a5e499fa6beb2b1888e2d1034b0
Emperor <panditom2003@gmail.com>
2025-04-21 15:05:07 +0530

    Created Custom Exceptions

---
+++ Assignment 8/CustomExceptions.java
@@ -0,0 +1,25 @@
+class EmptyFieldException extends Exception {
+    public EmptyFieldException(String message) {
+        super(message);
+    }
+}
+
+class DuplicatePRNException extends Exception {
+    public DuplicatePRNException(String message) {
+        super(message);
+    }
+}
+
+class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
+
+class CustomExceptions extends Exception {
+        public CustomExceptions(String message) {
+            super(message);
+    }
+}
+
+
