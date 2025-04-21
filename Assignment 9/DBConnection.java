f9b090db02f7746785aa45f20ddcb4e714fd1347
Emperor <panditom2003@gmail.com>
2025-04-22 02:23:40 +0530

    Run

--- Assignment 9/DBConnection.java
+++ Assignment 9/DBConnection.java
@@ -9,7 +9,14 @@ public class DBConnection {
     private static final String PASSWORD = "sit123";
 
     public static Connection getConnection() throws SQLException {
-         
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded successfully!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver loading failed: " + e.getMessage());
+        }
+        
+    
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
